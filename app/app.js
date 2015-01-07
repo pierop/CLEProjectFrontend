cfad5bc93c470c4e60b571ab245647dc895c6125
anais <anais@anais-MS-16G7>
2015-01-07 16:56:58 +0100

    anais | Pages created

--- app/app.js
+++ app/app.js
@@ -1,12 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var lanj = angular.module('lanjApp', ['ngRoute']);
+
+lanj.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+                .when('/provider', {
+                    templateUrl: 'providerView/provider.html'
+                })
+                .when('/admin', {
+                    templateUrl: 'adminView/admin.html'
+                })
+                .when('/professor', {
+                    templateUrl: 'professorView/professor.html'
+                })
+                .when('/student', {
+                    templateUrl: 'studentView/student.html'
+                })
+                .otherwise({redirectTo: '/view1'});
+    }]);
