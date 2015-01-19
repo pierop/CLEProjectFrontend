11e8d1fd3aaad4ded8328eaa453c6a00dc8d12e1
Anais <anais@anais-MS-16G7.(none)>
2015-01-19 09:51:29 +0100

    anais | merge

--- app/providerView/provider.js
+++ app/providerView/provider.js
@@ -140,7 +140,7 @@ lanj.controller('ProviderController', function ($scope, $location, backendFactor
             if ($scope.services.autoTemplates.selected) {
                 console.log("templates:");
                 for (var i = 0; i < $scope.services.autoTemplates.templates.length; i++) {
-                    console.log($scope.services.autoTemplates.templates[i]);
+                    console.log($scope.services.autoTemplates.templates[i].name);
                 }
             }
 
